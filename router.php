4b01f7f5e87822de9ea9143b1fb85c4b05f73819
Eugenio Miller <eugeniomiller93@gmail.com>
2020-05-20 23:16:21 -0300

    Al comentar, había un error y no me deslogueaba. Ya está solucionado

--- router.php
+++ router.php
@@ -106,16 +106,16 @@
             $controller = new UserController();
             $controller->showUserHome();
         break;
-        case 'librosAutorUser':
+        /* case 'librosAutorUser':
             $controller = new UserController();
             $controller->showBooksAuthorUser($parametros[1]); //-----FUINCÓN COMENTADA
         break;
         case "leido":
             $controller = new UserController();
             $controller->readBook($parametros[1]);//-----FUINCÓN COMENTADA
-        break;
+        break;*/
         case "logoutUser": 
-            $controller = new UserController();//-----FUINCÓN COMENTADA
+            $controller = new AdminController();//-----FUINCÓN COMENTADA
             $controller->logoutUser();
         break;
 
